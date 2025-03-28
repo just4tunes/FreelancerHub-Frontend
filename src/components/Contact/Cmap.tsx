63ba04201c08ec5c882e0cbacfad3bfa5820f4a4
Fortune <fortuneosuh04@gmail.com>
2025-03-28 13:30:31 +0100

    testing again

--- src/components/Contact/Cmap.tsx
+++ src/components/Contact/Cmap.tsx
@@ -1,24 +1,24 @@
-// import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-// import "leaflet/dist/leaflet.css";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
 
 
-// const Cmap = () => {
-//   const mapCenter: [number, number] = [4.8242, 7.0336]; // Port Harcourt Coordinates
-//   const mapZoom: number = 13;
+const Cmap = () => {
+  const mapCenter: [number, number] = [4.8242, 7.0336]; // Port Harcourt Coordinates
+  const mapZoom: number = 13;
 
-//   return (
-//     <div className="w-full h-[60vh] rounded-lg overflow-hidden">
-//       <MapContainer center={mapCenter} zoom={mapZoom} className="w-full h-full">
-//         <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-//         <Marker position={mapCenter}>
-//           <Popup>Port Harcourt, Nigeria</Popup>
-//         </Marker>
-//       </MapContainer>
-//     </div>
-//   );
-// };
+  return (
+    <div className="w-full h-[60vh] rounded-lg overflow-hidden">
+      <MapContainer center={mapCenter} zoom={mapZoom} className="w-full h-full">
+        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        <Marker position={mapCenter}>
+          <Popup>Port Harcourt, Nigeria</Popup>
+        </Marker>
+      </MapContainer>
+    </div>
+  );
+};
 
-// export default Cmap;
+export default Cmap;
 
 
 
